819aa324739e4a26255931628906af92e9d3c5a3
1000ch <shogo.sensui@gmail.com>
2014-12-10 01:03:01 +0900

    update

--- index.js
+++ index.js
@@ -8,7 +8,9 @@ var bodyParser = require('body-parser');
 var app = express();
 app.use(helmet());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 app.set('view engine', 'jade');
 app.set('views', path.join(__dirname, 'views'));
