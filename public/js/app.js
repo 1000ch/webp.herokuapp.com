64f84f8cfa21a876c7f3a6c2577b974a13cee988
1000ch <shogo.sensui@gmail.com>
2016-09-25 17:57:23 +0900

    ES2015ify

--- public/js/app.js
+++ public/js/app.js
@@ -1,22 +1,21 @@
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', () => {
+  const imageBefore = document.querySelector('#js-image-before');
+  const imageAfter = document.querySelector('#js-image-after');
+  const downloadBefore = document.querySelector('#js-download-before');
+  const downloadAfter = document.querySelector('#js-download-after');
 
-  var imageBefore    = document.querySelector('#js-image-before');
-  var imageAfter     = document.querySelector('#js-image-after');
-  var downloadBefore = document.querySelector('#js-download-before');
-  var downloadAfter  = document.querySelector('#js-download-after');
+  const input = document.querySelector('#js-input');
+  const drop = document.querySelector('#js-drop');
 
-  var input = document.querySelector('#js-input');
-  var drop  = document.querySelector('#js-drop');
-  
   drop.addEventListener('dragenter', onDragEnter);
   drop.addEventListener('dragstart', onDragStart);
   drop.addEventListener('dragover', onDragOver);
   drop.addEventListener('dragleave', onDragLeave);
   drop.addEventListener('dragend', onDragEnd);
   drop.addEventListener('drop', onDrop);
-  
-  var blobBefore = null;
-  var blobAfter = null;
+
+  let blobBefore = null;
+  let blobAfter = null;
 
   function onDragEnter (e) {
     this.classList.add('is-dragover');
@@ -41,24 +40,23 @@ document.addEventListener('DOMContentLoaded', function () {
   }
 
   function onDrop (e) {
-
     e.stopPropagation();
     e.preventDefault();
-    
+
     if (e.dataTransfer.files.length === 0) {
       return false;
     }
 
     blobBefore && URL.revokeObjectURL(blobBefore);
     blobAfter && URL.revokeObjectURL(blobAfter);
-    
-    var file = e.dataTransfer.files[0];
+
+    let file = e.dataTransfer.files[0];
     blobBefore = file.slice(0, file.size);
 
     downloadBefore.download = file.name;
     downloadAfter.download = file.name.replace(/(jpeg|jpg|png)/, 'webp');
 
-    sendBlob(blobBefore, function (xhr) {
+    sendBlob(blobBefore, xhr => {
       blobAfter = xhr.response;
       downloadAfter.href = imageAfter.src = URL.createObjectURL(blobAfter);
     });
@@ -66,31 +64,30 @@ document.addEventListener('DOMContentLoaded', function () {
     downloadBefore.href = imageBefore.src = URL.createObjectURL(blobBefore);
   }
 
-  input.addEventListener('change', function (e) {
-
+  input.addEventListener('change', e => {
     blobBefore && URL.revokeObjectURL(blobBefore);
     blobAfter && URL.revokeObjectURL(blobAfter);
 
-    var file = input.files[0];
+    let file = input.files[0];
     blobBefore = file.slice(0, file.size);
 
     downloadBefore.download = file.name;
     downloadAfter.download = file.name.replace(/(jpeg|jpg|png)/, 'webp');
 
-    sendBlob(blobBefore, function (xhr) {
+    sendBlob(blobBefore, xhr => {
       blobAfter = xhr.response;
       downloadAfter.href = imageAfter.src = URL.createObjectURL(blobAfter);
     });
 
     downloadBefore.href = imageBefore.src = URL.createObjectURL(blobBefore);
   });
-  
+
   function sendBlob (blob, callback) {
-    var xhr = new XMLHttpRequest();
+    const xhr = new XMLHttpRequest();
     xhr.open('POST', '/api/cwebp', true);
     xhr.responseType = 'blob';
     //xhr.responseType = 'arraybuffer';
-    xhr.onload = function (e) {
+    xhr.onload = e => {
       if (xhr.readyState === XMLHttpRequest.DONE) {
         //var arrayBuffer = new Uint8Array(xhr.response);
         callback(xhr);
